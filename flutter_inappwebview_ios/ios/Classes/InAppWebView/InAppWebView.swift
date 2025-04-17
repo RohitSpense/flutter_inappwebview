7559f6fe7af1cbba35f7ef6c4d2453f31d6d408e
Rohit <rohit.kumar@spense.money>
2025-04-17 17:48:26 +0530

    change for keyboard issue

--- flutter_inappwebview_ios/ios/Classes/InAppWebView/InAppWebView.swift
+++ flutter_inappwebview_ios/ios/Classes/InAppWebView/InAppWebView.swift
@@ -115,6 +115,30 @@ public class InAppWebView: WKWebView, UIScrollViewDelegate, WKUIDelegate,
             }
         }
     }
+
+    private var _scrollViewContentInsetAdjusted = false
+        @objc func keyboardWillShow(notification: NSNotification) {
+            // UIResponder.keyboardWillShowNotification will be fired also
+            // when changing focus between HTML inputs with the keyboard already open
+            if (scrollView.adjustedContentInset != .zero) {
+                // if resizeToAvoidBottomInset is false on Flutter side,
+                // scrollView.adjustedContentInset.bottom will be > 0
+                if scrollView.adjustedContentInset.bottom > 0 {
+                    // if the scrollView.contentInset has already been fixed, do nothing
+                    if !_scrollViewContentInsetAdjusted {
+                        _scrollViewContentInsetAdjusted = true
+                        let insetToAdjust = scrollView.adjustedContentInset
+                        scrollView.contentInset = UIEdgeInsets(top: -insetToAdjust.top, left: -insetToAdjust.left,
+                                                               bottom: -insetToAdjust.bottom, right: -insetToAdjust.right)
+                    }
+                } else {
+                    scrollView.contentInset = .zero
+                }
+            }
+        }
+        @objc func keyboardWillHide(notification: NSNotification) {
+            _scrollViewContentInsetAdjusted = false
+        }
     
     required public init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
@@ -345,6 +369,16 @@ public class InAppWebView: WKWebView, UIScrollViewDelegate, WKUIDelegate,
     }
 
     public func prepare() {
+
+        if #available(iOS 17.2, *) {
+                    // Fix https://github.com/pichillilorenzo/flutter_inappwebview/issues/1947
+                    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)),
+                                                           name: UIResponder.keyboardWillShowNotification,
+                                                           object: nil)
+                    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)),
+                                                           name: UIResponder.keyboardWillHideNotification,
+                                                           object: nil)
+                }
         scrollView.addGestureRecognizer(self.longPressRecognizer)
         scrollView.addGestureRecognizer(self.recognizerForDisablingContextMenuOnLinks)
         scrollView.addGestureRecognizer(self.panGestureRecognizer)
